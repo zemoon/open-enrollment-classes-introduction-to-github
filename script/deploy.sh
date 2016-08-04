3d110fd80ef4811ac2ec386ea0bf2ec9a467e0d7
Hector Alfaro <hectorsector@github.com>
2016-08-04 12:50:03 -0400

    change deploy script for testing purposes

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "about to deploy..."
-git config user.name "Travis CI User"
+git config user.name "Travis CI"
 git config user.email "noone@noone.com"
 git config push.default simple
-#git checkout -b cleanup-and-merge
-git checkout master
+git branch --all #which branch are we on?
+#git checkout master
 mv class-pins.topojson class-pins
 echo "I found new files, combining and pushing now!"
 geojson-merge *.topojson class-pins > class-pins.topojson
@@ -14,7 +14,6 @@ rm *.topojson
 mv class-pins class-pins.topojson
 git add -A
 git commit -m "[skip ci] clean up class files"
-#git merge --no-ff --no-edit cleanup-and-merge
 git remote set-url origin git@github.com:hectorsector/new-travel.git
-git push -u origin master
+git push # -u origin master
 echo "All done. Please go create the PR and merge if tests are passing!"
