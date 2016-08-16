49b52d226cbb34fb813722916c03d1d61da163b9
Briana Swift <brianamarie@github.com>
2016-08-16 17:32:30 -0500

    minor edits deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,6 +5,7 @@ git config user.email "trainingdemos+githubteacher@github.com"
 git config push.default simple # we don't want to push all our branches
 git branch --all #which branch are we on? for testing purposes
 git checkout master
+git checkout -b deploy-script-branch
 mv class-pins.topojson class-pins # preserves the old class-pins file
 echo "I found new files, combining and pushing now!"
 geojson-merge *.topojson class-pins > class-pins.topojson # merges any new topojson files with the existing pins file
@@ -14,6 +15,11 @@ rm *.topojson # remove all student topojson files
 mv class-pins class-pins.topojson # put the class pins file back
 git add -A
 git commit -m "[skip ci] clean up class files" # we don't want to trigger a build -- the reason we're doing this to begin with is because we're in the middle of a build
+git checkout master
 git remote set-url origin git@github.com:githubschool/open-enrollment-classes-introduction-to-github.git # Travis automatically clones via HTTPS -- this allows us to push using SSH
-git push -u origin master
+git checkout deploy-script-branch
+#may need to authenticate with oauth here
+POST /repos/:githubschool/:open-enrollment-classes-introduction-to-github/merges
+git checkout master
+git branch -d deploy-script-branch
 echo "All done."
