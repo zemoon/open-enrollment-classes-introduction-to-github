e65f1ac5b893115e6f9b762975941f610c9314f4
Briana Swift <brianamarie@github.com>
2016-08-16 18:36:35 -0500

    introduce kyles suggesteions to exit CI if no topojson files

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 echo "about to deploy..."
+
+# Get list of new topojson files
+geojsonfiles=$(find . -name '*.topojson' -not -name 'class_pins.topojson')
+
+# If there are no files to build/deploy, we exit the build
+if [ -z "$geojsonfiles" ]; then
+  echo "No new files found. Aborting deployment..."
+  exit 0
+else
+  echo "topojson files found:"
+  echo $geojsonfiles
+fi
 git config user.name "GitHub Teacher" # set the user to Travis CI so we know this wasn't done by hu-man
 git config user.email "trainingdemos+githubteacher@github.com"
 git config push.default simple # we don't want to push all our branches
 git branch --all #which branch are we on? for testing purposes
 git checkout master
-git checkout -b deploy-script-branch
 mv class-pins.topojson class-pins # preserves the old class-pins file
 echo "I found new files, combining and pushing now!"
 geojson-merge *.topojson class-pins > class-pins.topojson # merges any new topojson files with the existing pins file
@@ -14,12 +25,7 @@ mv class-pins.topojson class-pins # preserve the newly created file
 rm *.topojson # remove all student topojson files
 mv class-pins class-pins.topojson # put the class pins file back
 git add -A
-git commit -m "[skip ci] clean up class files" # we don't want to trigger a build -- the reason we're doing this to begin with is because we're in the middle of a build
-git checkout master
+git commit -m "Automatic commmit: merge topojson files" # we don't want to trigger a build -- the reason we're doing this to begin with is because we're in the middle of a build
 git remote set-url origin git@github.com:githubschool/open-enrollment-classes-introduction-to-github.git # Travis automatically clones via HTTPS -- this allows us to push using SSH
-git checkout deploy-script-branch
-#may need to authenticate with oauth here
-POST /repos/:githubschool/:open-enrollment-classes-introduction-to-github/merges
-git checkout master
-git branch -d deploy-script-branch
+git push -u origin master
 echo "All done."
