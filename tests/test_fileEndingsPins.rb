9ae4e242a4516d30feaab4ebbf78884c64b27b47
Briana Swift <brianamarie@github.com>
2016-10-29 07:43:47 -0500

    adding the ruby test files

---
+++ tests/test_fileEndingsPins.rb
@@ -0,0 +1,12 @@
+require 'test/unit'
+
+class TestFileTypePins < Test::Unit::TestCase
+
+  def test_filecount
+    Dir.foreach('./_pins/') do |item|
+      next if item == '.' or item == '..'
+        file_exist = File.fnmatch?('*.yaml', item)
+        assert_equal(true, file_exist)
+      end
+    end
+  end
